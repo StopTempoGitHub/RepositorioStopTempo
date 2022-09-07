2da074713b498ec35a7177485bdd1ab9658e19b8
StopTempo <gette_f@live.com>
2022-09-07 15:09:21 -0300

    28vo Commit Correccion del texto de Swal

--- js/swal.js
+++ js/swal.js
@@ -3,7 +3,7 @@ const formularioContacto = document.querySelector('.formularioContacto');
 formularioContacto.addEventListener('click', () => {
     swal({
         title: "¡Contacto No Funcional!",
-        text: "Lo siento, pero por el momento el formulario de contacto no es funcional, pero no te preocupes, podes contactarte conmigo por: \n Correo: fernando.gette@eaf.edu.ar \n Whatsapp: 11-6270-2307 \n Instagram: @stoptempo \n ¡Pronto estaré agregando funcionalidad al formulario!",
+        text: "Lo siento, por el momento el formulario de contacto no es funcional, pero no te preocupes, podes contactarte conmigo por: \n Correo: fernando.gette@eaf.edu.ar \n Whatsapp: 11-6270-2307 \n Instagram: @stoptempo \n ¡Pronto estaré agregando funcionalidad al formulario!",
         button: "OK",
       });
 });
