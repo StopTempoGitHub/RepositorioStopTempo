f108ea7c0e68c624178f27b02b1c02e34f13b9ba
StopTempo <gette_f@live.com>
2022-09-07 13:41:20 -0300

    27vo Commit SweetAlert al Formulario

---
+++ js/swal.js
@@ -0,0 +1,9 @@
+const formularioContacto = document.querySelector('.formularioContacto');
+
+formularioContacto.addEventListener('click', () => {
+    swal({
+        title: "¡Contacto No Funcional!",
+        text: "Lo siento, pero por el momento el formulario de contacto no es funcional, pero no te preocupes, podes contactarte conmigo por: \n Correo: fernando.gette@eaf.edu.ar \n Whatsapp: 11-6270-2307 \n Instagram: @stoptempo \n ¡Pronto estaré agregando funcionalidad al formulario!",
+        button: "OK",
+      });
+});
